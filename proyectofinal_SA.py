c6a8030e036370e739ac4c996613373569bd7147
Stronauta <samircastillop@gmail.com>
2026-03-27 20:19:03 -0400

    first commit

---
+++ proyectofinal_SA.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# Configuración de MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'bd_proyecto'
+
+mysql = MySQL(app)
